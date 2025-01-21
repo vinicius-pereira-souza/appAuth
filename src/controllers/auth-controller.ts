43ab4111a65e3b38b5834f13dfa65aa9345e920a
Vinicius Pereira de Souza <vinicius.pereira.souza98@gmail.com>
2025-01-21 16:38:32 -0300

    feat: função de signup

--- src/controllers/auth-controller.ts
+++ src/controllers/auth-controller.ts
@@ -2,9 +2,60 @@ import { Request, Response } from "express";
 import { signupFormSchema } from "../lib/validations";
 import { hashSync, compareSync } from "bcrypt";
 import { createSession } from "../lib/session";
+import { PrismaClient } from "@prisma/client";
 
-export async function signup(req: Request, res: Response) {}
+const prisma = new PrismaClient();
 
-export async function login(req: Request, res: Response) {}
+export async function signup(req: Request, res: Response) {
+  try {
+    const { error, data } = signupFormSchema.safeParse(req.body);
+
+    if (error) {
+      return res.status(400).json({ error: error.flatten().fieldErrors });
+    }
+
+    const userExist = await prisma.user.findFirst({
+      where: { OR: [{ email: data.email }, { name: data.username }] },
+    });
+
+    if (userExist) {
+      return res
+        .status(400)
+        .json({ message: "the username or email already exists" });
+    }
+
+    const { username, email, password } = data;
+    const passwordHash = hashSync(password, 10);
+
+    const user = await prisma.user.create({
+      data: { email, password: passwordHash, name: username },
+    });
+
+    if (!user) {
+      return res
+        .status(500)
+        .json({ message: "An error has occurred, please try again later" });
+    }
+
+    await createSession(user.id, req);
+    return res
+      .status(201)
+      .json({ message: "authentication successful, we will redirect you" });
+  } catch (error) {
+    console.log("Error during registration: ", error);
+    return res
+      .status(500)
+      .json({ message: "An error has occurred, please try again later" });
+  }
+}
+
+export async function login(req: Request, res: Response) {
+  try {
+    const test = await prisma.user.findMany();
+    console.log(test);
+  } catch (error) {
+    console.log("ocorreu um erro: ", error);
+  }
+}
 
 export async function logout(req: Request, res: Response) {}
